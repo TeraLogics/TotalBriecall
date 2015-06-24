11bf93a9319ba2549e6433bd1531404155b2c9ea
Dillon Bussert <bussert@teralogics.com>
2015-06-24 14:23:54 -0400

    Added state map to details

--- public/js/app-recall.js
+++ public/js/app-recall.js
@@ -7,10 +7,39 @@ requirejs.config({
 	},
 	paths: {
 		jquery: 'jquery-2.1.4.min',
-		bootstrap: 'bootstrap.min'
+		bootstrap: 'bootstrap.min',
+		underscore: 'underscore-min',
+		UsStates: 'us-states'
 	}
 });
 
-requirejs(['jquery', 'bootstrap'], function ($, bootstrap) {
+requirejs(['jquery', 'bootstrap', 'underscore', 'MapApp', 'UsStates'], function ($, bootstrap, _, MapApp, UsStates) {
+	var map = new MapApp({
+		height: 300,
+		width: 600,
+		div: 'map'
+	});
 
+	map.create();
+	
+	var style = function (feature) {
+		return {
+			fillColor: '#800026', //feature.properties.name ? '#800026' : null,
+			weight: 1,
+			opacity: 1,
+			color: 'white',
+			//dashArray: '3',
+			fillOpacity: 0.7
+		};
+	};
+
+	if (states) {
+		map.add('layer', UsStates.getStateGeoJSON(states), {
+			style: style
+		});
+	} else {
+		map.add('layer', UsStates.getNationGeoJSON(), {
+			style: style
+		});
+	}
 });
