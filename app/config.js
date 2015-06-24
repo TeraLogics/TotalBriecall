a02907acf8133ff8e481bdfc0b590fbd14b6ba15
Michael Bell <mbell8903@yahoo.com>
2015-06-24 18:23:39 -0400

    Fixed issue #3

--- app/config.js
+++ app/config.js
@@ -6,5 +6,6 @@ module.exports = _.extend({
 	NODE_ENV: 'production',
 	SECRET: 'foobarbaz',
 	SESSIONLENGTH: 3600000,
-	PORT: 5000
+	PORT: 5000,
+	MONGOLAB_URI: 'mongodb://localhost'
 }, process.env);
